16bccc13d6048e960e6a8662bd271bd4d7687fa1
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2023-10-19 13:36:26 -0700

    "

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,4 +12,5 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 int _putchar(char c);
 void reverse_array(int *a, int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 #endif
