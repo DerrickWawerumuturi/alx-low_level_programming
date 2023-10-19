e7e3e106817dc94091cb5fabe5c9eeb07f6a3914
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2023-10-19 13:38:54 -0700

    buffer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,4 +13,5 @@ int _strcmp(char *s1, char *s2);
 int _putchar(char c);
 void reverse_array(int *a, int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 #endif
