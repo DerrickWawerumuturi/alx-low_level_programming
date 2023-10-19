9b82fbe9f983d196de2344ee914f60ed8c69b424
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2023-10-19 11:27:34 -0700

    encoding

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - replaces letters with digits
+ * @str: string to be replaced
+ * Return: a pointer is  returned
+ */
+
+char *leet(char *str)
+{
+	int loopInput;
+	int loopString;
+
+	char replacing[] = "aAeEoOtTlL";
+	char replacement[] = "443007711";
+
+	for (loopInput = 0 ; str[loopInput] != '\0' ; loopInput++)
+	{
+		for (loopString = 0 ; replacing[loopString] != '\0'; loopString++)
+		{
+			if (str[loopInput] == replacing[loopString])
+			{
+				str[loopInput] = replacement[loopString];
+				break;
+			}
+		}
+	}
+	return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 char *_strcat(char *dest, char *src);
 char *string_toupper(char *);
+char *leet(char *);
 char *cap_string(char *);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
