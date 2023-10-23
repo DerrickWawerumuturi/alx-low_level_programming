d869b8115fa66a15e8faaa4ac2173da6b5b146f6
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2023-10-23 11:22:03 -0700

    print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,9 +9,9 @@ void print_chessboard(char (*a)[8])
 	int p;
 	int z;
 
-	for (p = 0 ; p < 0 ; p++)
+	for (p = 0 ; p < 8 ; p++)
 	{
-		for (z = 0 ; z < 0 ; z++)
+		for (z = 0 ; z < 8 ; z++)
 		{
 			_putchar(a[p][z]);
 		_putchar('\n');
