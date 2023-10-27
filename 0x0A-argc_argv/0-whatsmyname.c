0448e3049f3cd068e2668f35dde353d2ac748ef2
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2023-10-27 13:28:08 -0700

    new name is

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,13 +4,13 @@
  * main -prints out the name of the program
  * @argc:argument count
  * @argv:argument vector
- * Return:0 on success 
+ * Return:0 on success
  */
 
 int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("%s\n" , argv[0]);
-	return (0);	
-	
+	printf("%s\n", argv[0]);
+	return (0);
+
 }
