2afa6d80638b16e63aabaec405f635d88fb859ce
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2023-10-27 13:25:21 -0700

    new name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,19 +4,13 @@
  * main -prints out the name of the program
  * @argc:argument count
  * @argv:argument vector
- * Return:0 on success , 1 on failure
+ * Return:0 on success 
  */
 
 int main(int argc, char *argv[])
 {
-	if (argc > 0)
-	{
-		printf("%s\n" , argv[0]);
-		return (0);
-	}
-	else
-	{
-		printf("%s:there is no argument passed\n", argv[0]);
-		return (1);
-	}
+	(void)argc;
+	printf("%s\n" , argv[0]);
+	return (0);	
+	
 }
