4334010afdc8bc750acfebf929cb14b0f60b446b
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2023-11-02 13:45:04 -0700

    chat

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,39 +1,47 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-/**
- * main - print the mul of args passed
- * @argc: argument count
- * @argv: array with the args
- * Return: 0 (Success)
- */
-
-int main(int argc, char **argv)
+int is_digit(const char *str)
 {
-	int i, j;
-	unsigned long int mul, num1, num2;
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+			return (0);
+		str++;
+	}
+	return (1);
+}
 
+long multiply(long num1, long num2)
+{
+	return num1 * num2;
+}
+
+int main(int argc, char *argv[])
+{
 	if (argc != 3)
 	{
 		printf("Error\n");
-		exit(98);
+		return 98;
 	}
 
-	for (i = 1; i < argc; i++)
+	if (!is_digit(argv[1]) || !is_digit(argv[2]))
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] < 48 || argv[i][j] > 57)
-			{
-				printf("Error\n");
-				exit(98);
-			}
-		}
+		printf("Error\n");
+		return 98;
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	mul = num1 * num2;
-	printf("%lu\n", mul);
-	return (0);
+
+	long num1 = strtol(argv[1], NULL, 10);
+	long num2 = strtol(argv[2], NULL, 10);
+
+	if (num1 <= 0 || num2 <= 0)
+	{
+		printf("Error\n");
+		return 98;
+	}
+
+	long result = multiply(num1, num2);
+	printf("%ld\n", result);
+	return 0;
 }
+
