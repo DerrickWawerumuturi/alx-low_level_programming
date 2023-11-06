a26c5ecbc4b722390cbce1f121cc6643c3bd4495
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2023-11-06 05:56:15 -0800

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - contains data types
+ * @name:first item
+ * @age:second item
+ * @owner: third item
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
