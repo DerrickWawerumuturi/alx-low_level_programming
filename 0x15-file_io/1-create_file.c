e702e891e3c67e04d31482c718165ec4595be633
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2023-11-27 11:43:13 -0800

    creates file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,29 +9,27 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, id = 0;
+	int fd, id = 0, tot = 1;
 
 	if (!filename)
 	{
 		return (-1);
 	}
-	if (!text_content)
-	{
-		return (-1);
-	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fd < 0)
-	{
+	if (fd == -1)
 		return (-1);
-	}
-	while (text_content[id] != '\0')
-		id++;
-	if (write(fd, text_content, id) != id)
+	if (text_content)
 	{
-		close(fd);
-		return (-1);
+		while (text_content[id])
+		{
+			id++;
+		}
+		tot = write(fd, text_content, id);
+
 	}
+	if (tot == -1)
+		return (-1);
 	close(fd);
 	return (1);
 }
